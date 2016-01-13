ddf48f5a3bffd246f286350dfe465a24223ea7ca
Joshua Graham <jgcomms@yahoo.com.au>
2016-01-13 23:05:27 +1100

    mistake on favicon link

--- includes/templates/templatePageBlank.php
+++ includes/templates/templatePageBlank.php
@@ -3,7 +3,7 @@
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="shortcut icon" href="<?php echo CONFIG_ROOT_URL."/favicon.ico"; ?>" type="image/x-icon" />
+<link rel="shortcut icon" href="<?php echo CONFIG_ROOT_URL."favicon.ico"; ?>" type="image/x-icon" />
 <!-- CSS files -->
 <?php
 foreach ($this->stylesheets as $stylesheet) {
